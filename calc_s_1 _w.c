fd006788a60fa7118163ff5d1d4004fda154fbeb
Michał Kiełkowski <mkielkowski01@gmail.com>
2024-06-19 05:17:13 +0200

    ok

---
+++ calc_s_1 _w.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+int main() {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <number_of_points>\n", argv[0]);
+        return 1;
+    }
+    long npoints = atol(argv[1]);
+    long circle_count = 0;
+    double xcoordinate, ycoordinate;
+    clock_t start, end;
+    double cpu_time_used;
+    int size = 1;
+
+    // Inicjalizacja generatora liczb pseudolosowych
+    srand(time(NULL));
+
+    start = clock(); // Rozpoczęcie pomiaru czasu
+
+    for (long j = 0; j < npoints; j++) {
+        // Generowanie dwóch liczb losowych w zakresie [0, 1]
+        xcoordinate = (double)rand() / RAND_MAX;
+        ycoordinate = (double)rand() / RAND_MAX;
+
+        // Sprawdzenie, czy punkt znajduje się wewnątrz koła (o promieniu 0.5)
+        if (xcoordinate * xcoordinate + ycoordinate * ycoordinate <= 1) {
+            circle_count++;
+        }
+    }
+
+    end = clock(); // Zakończenie pomiaru czasu
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    // Obliczenie przybliżonej wartości liczby pi
+    double pi = 4.0 * circle_count / npoints;
+
+    // Sprawdzenie, czy wyliczona wartość liczby π znajduje się w zakresie [0.4]
+    if (pi >= 0.4 && pi <= 4.0) {
+        printf("Liczba PI obliczona metoda Monte Carlo: %f\n", pi);
+    } else {
+        printf("Błąd: Wyliczona wartość liczby PI (%f) nie mieści się w zakresie [0.4, 4.0]\n", pi);
+    }
+
+    printf("Liczba punktów: %ld\n", npoints); // Dodatkowy wiersz wydruku
+    printf("Czas wykonania: %f sekund\n", cpu_time_used);
+    printf("Liczba użytych procesorów: %d\n", size);
+
+    return 0;
+}
---
+++ w_script.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+echo "Starting the script to compile and run MPI programs..."
+
+# Updated program array with new suffix
+all_programs=(calc_p_1_w.c calc_p_10_w.c calc_p_60_w.c calc_s_1_w.c calc_s_10_w.c calc_s_60_w.c)
+
+# Initialize or clear the CSV file
+echo "PI, Points, Execution Time, Processors" > results_w.csv
+
+# Loop through each program
+for program in "${all_programs[@]}"
+do
+    # Extract the base name without the .c extension
+    base_name=$(echo "$program" | cut -f 1 -d '.')
+
+    # Compile the program using mpicc
+    mpicc -o "${base_name}_c" "$program"
+
+    # Determine the base number of points based on the file name
+    if [[ $program == *"1_w.c" ]]; then
+        base_npoints=61500000 # For programs ending with 1
+    elif [[ $program == *"10_w.c" ]]; then
+        base_npoints=615000000 # For programs ending with 10
+    elif [[ $program == *"60_w.c" ]]; then
+        base_npoints=4270000000 # For programs ending with 60
+    fi
+
+    # Determine the correct execution based on the program type
+    if [[ $program == calc_s_* ]]; then
+        # Sequential programs, executed with -np 1
+        output=$(mpiexec -machinefile ./1_node_with_8_process -np 1 "./${base_name}_c" $base_npoints)
+    else
+        # Parallel programs, executed with varying numbers of processors from 2 to 8
+        for np in {2..8}
+        do
+            # Weak scaling: scale npoints with the number of processors
+            npoints=$(($base_npoints * $np))
+            output=$(mpiexec -machinefile ./1_node_with_8_process -np "$np" "./${base_name}_c" $npoints)
+            # Extract data from the output and append to the CSV file
+            pi=$(echo "$output" | grep -o "Liczba PI obliczona metoda Monte Carlo: [0-9.]*" | awk '{print $NF}')
+            points=$(echo "$output" | grep -o "Liczba punktów: [0-9]*" | awk '{print $NF}')
+            time=$(echo "$output" | grep -o "Czas wykonania: [0-9.]*" | awk '{print $NF}')
+            processors=$(echo "$output" | grep -o "Liczba użytych procesorów: [0-9]*" | awk '{print $NF}')
+
+            if [[ -n "$pi" && -n "$points" && -n "$time" && -n "$processors" ]]; then
+                echo "$pi, $points, $time, $processors" >> results_w.csv
+            fi
+        done
+    fi
+done
+
+echo "Script has completed execution."
